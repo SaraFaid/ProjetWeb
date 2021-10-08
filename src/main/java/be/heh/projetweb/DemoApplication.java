ea831b7c5ce80779644c31c459f119b450a9b682
SaraFaid <50913811+SaraFaid@users.noreply.github.com>
2021-10-08 14:45:27 +0200

    Update DemoApplication.java

--- src/main/java/be/heh/projetweb/DemoApplication.java
+++ src/main/java/be/heh/projetweb/DemoApplication.java
@@ -19,7 +19,7 @@ public class DemoApplication {
     }
 
     @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+    public String hello(@RequestParam(value = "name", defaultValue = "Tobi") String name) {
+        return String.format("Hoi, I'm %s!", name);
     }
 }
